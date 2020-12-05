3f59cb2d36a06926bf2ec8569f05bef1dc475c0d
Mariano <infernuzigt@gmail.com>
2020-12-05 16:03:29 -0300

    Example for EventController

---
+++ iFocus/Assets/Scripts/ApplicationControllers/EventController/ExampleEvent.cs
@@ -0,0 +1,39 @@
+﻿using Events; // Required
+using UnityEngine;
+
+public class ExampleEvent : MonoBehaviour
+{
+    #region Send
+
+    private GameEvent _event;
+
+    private void Start()
+    {
+        _event = new GameEvent();
+        // _event.Variable
+
+        EventController.TriggerEvent(_event);
+    }
+
+    #endregion
+
+    #region Receive
+
+    private void OnEnable()
+    {
+        EventController.AddListener<GameEvent>(OnTestFunction);
+    }
+    
+    private void OnDisable()
+    {
+        EventController.RemoveListener<GameEvent>(OnTestFunction);
+    }
+    
+    private void OnTestFunction(GameEvent evt)
+    {
+        
+    }
+
+    #endregion
+
+}
